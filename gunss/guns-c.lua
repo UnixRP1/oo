da8db9c30fc1b07a01eb3d8921cefb8a759a3c94
UnixRP1 <81623677+UnixRP1@users.noreply.github.com>
2021-04-12 07:11:45 +0100

    Add files via upload

---
+++ gunss/guns-c.lua
@@ -0,0 +1,37 @@
+RegisterCommand("clear", function()
+    RemoveAllPedWeapons(GetPlayerPed(-1), true)
+    notify("~r~Cleared All Weapons")
+end)
+
+-- Spawns a random car from the array or "table"
+local cars = {"adder", "comet", "gt86trust", "faggio"}
+RegisterCommand("car", function()
+    local car = (cars[math.random(#cars)])
+    spawnCar(car)
+    notify("~p~Spawned car: ~h~~g~" .. car)
+end)
+
+RegisterCommand("die", function()
+    SetEntityHealth(PlayerPedId(), 0)
+    notify("~r~ You Died Shitter.")
+end)
+
+Citizen.CreateThread(function()
+
+    local h_key = 74
+    while true do
+        Citizen.Wait(1)
+        if IsControlJustReleased(1, h_key) then
+            print("The key ".. h_key .. " was pressed")
+            giveWeapon("weapon_mosin")
+            giveWeapon("weapon_ak200")
+
+            giveWeapon("weapon_pumpshotgun")
+                weaponComponent("weapon_pumpshotgun", "COMPONENT_AT_AR_FLSH")
+
+            giveWeapon("weapon_knife")
+            alert("~b~Given Weapons with ~INPUT_VEH_HEADLIGHT~")
+        end
+    end
+
+end)
